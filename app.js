e9633fb3d7b9bf20175fb3aeb3c8c5317512c171
Emre ÇAĞLAR <emre52@gmail.com>
2016-02-26 11:08:59 +0200

    add sample home controller

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 'use strict';
 (function(){
-  angular.module('main', ['ngMaterial']);
+  angular.module('main', ['ngMaterial'])
+  .controller('homeController', function() {
+    var vm  = this;
+    var imagePath = 'img/list/60.jpeg';
+    vm.messages = [{
+      face : imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }];
+  });
 }())
